48a496b439cf818cf04d4b54322d72c0d93e6b4c
Sharayu12 <69318155+Sharayu12@users.noreply.github.com>
2020-08-10 02:16:22 +0530

    FE : NavBar styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React from "react";
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Tooltip from '@material-ui/core/Tooltip'
 import Button from '@material-ui/core/Button'
 import withStyles from '@material-ui/core/styles/withStyles'
+import DashboardIcon from '@material-ui/icons/Dashboard'
+import HomeIcon from '@material-ui/icons/Home'
+
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import logo from '../assets/immunize_logo.gif'
@@ -17,17 +21,28 @@ const styles = (theme) => ({
     root: {
         flexGrow: 1,
     },
-    menuButton: {
+    logo : {
         marginRight: theme.spacing(2),
     },
+    immunize : {
+        fontFamily: 'Bebas Neue',
+        fontSize : '35px',
+        marginTop : '5px',
+        flexGrow: 1
+    },
     login: {
-        // flex : 1,
-        // alignItems : 'right',
-        fontSize : '20px',
-        marginRight : '10px'
+        fontSize : '17px',
+        textTransform : 'capitalize',
+        fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
     },
     profile : {
-        borderRadius : '5px'
+        width: theme.spacing(4),
+        height: theme.spacing(4),
+        objectFit: 'cover',
+        borderRadius: '50%',
+    },
+    profileB : {
+        marginRight : '15px'
     }
 })
 
@@ -75,33 +90,54 @@ const NavBar = (props) => {
 
     return (
         <div >
-            <AppBar position="relative" color="transparent" style={{marginBottom: '10px'}}>
+            <AppBar position="relative" color="transparent" >
                 <Toolbar style={{ height: 50}}>
 
-                    <img src={logo} alt="Profile" width="130" height="50" />
+                    {/* logog */}
+                    <img src={logo} alt="Profile" width="30" height="30" className={classes.logo}/>
 
+                    {/* immunize title */}
+                    <div className={classes.immunize}>
+                        IMMUNIZE
+                    </div>
+
+                    {/* login */}
                     {!isAuthenticated && (
                         <Button className={classes.login} color="primary" onClick={() => loginWithRedirect()}>
                             Login
                         </Button>)}
                     
+                    {/* signup */}
                     {!isAuthenticated && (
                         <Button className={classes.login} color="primary" onClick={() => loginWithRedirect()}>
                             Signup
                         </Button>)}
 
+                    {/* home */}
                     {isAuthenticated && ( 
-                        <Button component = {Link} to="/profile">
-                            <img src={user.picture} alt="Profile" width="50" classname={classes.profile} />
-                        </Button>
+                        <Tooltip title="Home" >
+                            <Button color="primary" component = {Link} to="/home" >
+                                <HomeIcon/>
+                            </Button>
+                        </Tooltip>
                     )}
 
+                    {/* dashboard */}                        
                     {isAuthenticated && ( 
-                        <Button color="secondary" component = {Link} to="/home" >Home</Button>
+                        <Tooltip title="Dashboard" >
+                            <Button color="primary" component = {Link} to="/dashboard" >
+                                <DashboardIcon/> 
+                            </Button>
+                        </Tooltip>
                     )}
-                        
+
+                    {/* profile pic */}
                     {isAuthenticated && ( 
-                        <Button color="secondary" component = {Link} to="/dashboard" >Dashboard</Button>
+                        <Tooltip title="Profile" >
+                            <Button color="primary" component = {Link} to="/profile" className={classes.profileB}  >
+                                <img className={classes.profile} src={user.picture}  alt="Profile"/>
+                            </Button>
+                        </Tooltip>
                     )}
                 </Toolbar>
             </AppBar>
